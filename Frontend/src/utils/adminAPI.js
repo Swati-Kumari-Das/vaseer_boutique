80ca5c3abf712823465f6780704d03345efe68d6
Swati-Kumari-Das <swatikumaridas1111@gmail.com>
2025-07-25 10:01:27 +0530

    changed base URL

--- Frontend/src/utils/adminAPI.js
+++ Frontend/src/utils/adminAPI.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const adminAPI = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
